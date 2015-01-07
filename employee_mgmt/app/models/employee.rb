aa39291e8b0561de7fc343c7abb6e1aa1bd5c67a
bipashant <bibeksharma@lftechnology.com>
2015-01-07 14:52:15 +0545

    Qns one to 5 finished

--- employee_mgmt/app/models/employee.rb
+++ employee_mgmt/app/models/employee.rb
@@ -1,9 +1,24 @@
 class Employee < ActiveRecord::Base
 
-  def update_phoneno val
+ validates :first_name, presence:true
+  validates :last_name, presence: true
+  validates :phone,allow_blank: true, format: { with: /9[0-9]{9}/,message:"Must have exactly 10 digits number starting with 9"}
+ validates :salary, numericality: {greater_than: 0}
+ validates :email,uniqueness: true,allow_blank: true, format: { with: /[a-zA-Z]+@[a-z0-9A-Z]+.[.a-zA-Z]+/,message:"%{value}Invalid Email ID"}
+
+ after_validation(on: :create) do
+   self.first_name=self.first_name.capitalize
+   if(self.middle_name)
+      self.middle_name=self.middle_name.capitalize
+   end
+   self.last_name=self.last_name.capitalize
+
+ end
+
+
+ def update_phoneno val
     self.phone=val
     self.save
-    puts "Changed"
   end
 
   def display_all_employee
---
+++ employee_mgmt/db/migrate/20150107073337_change_phone_to_string_from_integer.rb
@@ -0,0 +1,5 @@
+class ChangePhoneToStringFromInteger < ActiveRecord::Migration
+  def change
+    change_column :employees, :phone, :string
+  end
+end
