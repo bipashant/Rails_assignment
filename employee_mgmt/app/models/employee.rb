0a5515ebdf58b32350460a5c65c4982906cefb31
bipashant <bibeksharma@lftechnology.com>
2015-01-08 09:35:29 +0545

    Day3 assignment after mentors review

--- employee_mgmt/app/models/employee.rb
+++ employee_mgmt/app/models/employee.rb
@@ -1,17 +1,15 @@
 class Employee < ActiveRecord::Base
 
- validates :first_name, presence:true
-  validates :last_name, presence: true
-  validates :phone,allow_blank: true, format: { with: /9[0-9]{9}/,message:"Must have exactly 10 digits number starting with 9"}
- validates :salary, numericality: {greater_than: 0}
- validates :email,uniqueness: true,allow_blank: true, format: { with: /[a-zA-Z]+@[a-z0-9A-Z]+.[.a-zA-Z]+/,message:"%{value}Invalid Email ID"}
+ validates :first_name,:last_name, presence:true
 
- after_validation(on: :create) do
-   self.first_name=self.first_name.capitalize
-   if(self.middle_name)
-      self.middle_name=self.middle_name.capitalize
-   end
-   self.last_name=self.last_name.capitalize
+  validates :phone,allow_blank: true, format: { with: /\A9.\d{8}\z/,message: "Must have exactly 10 digits number starting with 9"}
+ validates :salary, numericality: {greater_than: 0}
+ validates :email,uniqueness: true,allow_blank: true, format: { with: /[a-zA-Z]+@[a-z0-9A-Z]+.[.a-zA-Z]+/,message: "%{value}Invalid Email ID"}
+
+ after_validation on: :create do
+   first_name.capitalize!
+   middle_name.capitalize! if middle_name
+   last_name.capitalize!
 
  end
 
@@ -23,7 +21,8 @@ before_destroy :ask_user_for_delete_confirmation
  def ask_user_for_delete_confirmation
    puts "Are you sure to delete data?(Y/N)"
    input = gets.chomp()
-   if !(input == 'y')
+
+   unless input == 'y'
      puts "Record not deleted"
      return false
 
@@ -32,8 +31,8 @@ before_destroy :ask_user_for_delete_confirmation
 
 
  def update_phoneno val
-    self.phone=val
-    self.save
+    phone = val
+    save
   end
 
   def self.display_all_employee
